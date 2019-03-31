fd0dc319944bc687cdc4ba59cd3144106d258528
Wisnu Pramadhitya Ramadhan <wisnu.pramadhitya@ui.ac.id>
2019-03-31 08:55:36 +0700

    add some dependencies

---
+++ src/components/stables/Editor/index.jsx
@@ -0,0 +1,5 @@
+// https://github.com/outline/rich-markdown-editor
+// https://yarnpkg.com/en/package/slate-no-empty
+// https://yarnpkg.com/en/package/slate-paste-linkify
+// https://yarnpkg.com/en/package/slate-mark-hotkeys
+// https://yarnpkg.com/en/package/slate-drop-or-paste-images
