9558070038178585b62041173c4e6b25b61a4d79
Astari Dwi R <astaridwir@gmail.com>
2019-04-08 14:28:19 +0700

    progress

--- src/modules/api/helios/v1/channel.js
+++ src/modules/api/helios/v1/channel.js
@@ -10,18 +10,9 @@ export default Object.freeze({
     http.get(`${API_V1_URL}/channels/${channelId}/chants/${chantId}`),
   getListChannel: () => http.get(`${API_V1_URL}/channels`),
   getListChantUser: userId => http.get(`${API_V1_URL}/users/${userId}/chants`),
-  getChannelRequestList: userId =>
-    http.get(`${API_V1_URL}/users/${userId}/channel-requests/`),
-  getChannelRequestDetail: (userId, channelId) =>
-    http.get(`${API_V1_URL}/users/${userId}/channel-requests/${channelId}`),
-    getTimeline: () => http.get(`${API_V1_URL}/timeline`),
-    getChantDetail: (userId, chantId) => http.get(`${API_V1_URL}/users/${userId}/chants/${chantId}`),
-    createChannelRequest: (userId, coverImgUrl, title, description) =>
-        http.post(`${API_V1_URL}/users/${userId}/channel-requests/`, {
-            coverImgUrl,
-            title,
-            description
-        }),
+  getTimeline: () => http.get(`${API_V1_URL}/timeline`),
+  getChantDetail: (userId, chantId) =>
+    http.get(`${API_V1_URL}/users/${userId}/chants/${chantId}`),
   getChantDetail: (userId, chantId) =>
     http.get(`${API_V1_URL}/users/${userId}/chants/${chantId}`),
   postChant: (userId, channel, parentChant, title, body) =>
@@ -37,5 +28,21 @@ export default Object.freeze({
       body
     }),
   deleteChant: (userId, chantId) =>
-    http.delete(`${API_V1_URL}/users/${userId}/chants/${chantId}`)
+    http.delete(`${API_V1_URL}/users/${userId}/chants/${chantId}`),
+  getChannelRequestList: userId =>
+    http.get(`${API_V1_URL}/users/${userId}/channel-requests/`),
+  getChannelRequestDetail: (userId, channelId) =>
+    http.get(`${API_V1_URL}/users/${userId}/channel-requests/${channelId}`),
+  createChannelRequest: (userId, coverImgUrl, title, description) =>
+    http.post(`${API_V1_URL}/users/${userId}/channel-requests/`, {
+      coverImgUrl,
+      title,
+      description
+    }),
+   updateChannelRequest: (userId, channelId, coverImgUrl, title, description) =>
+   http.patch(`${API_V1_URL}/users/${userId}/channel-requests/${channelId}`, {
+       coverImgUrl,
+       title,
+       description
+   }), 
 });
