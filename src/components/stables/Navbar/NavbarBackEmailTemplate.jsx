4fd0ee31513a6bc041426c755fc21ba59a8195af
Nabil Tegar <ntegar30@gmail.com>
2019-05-16 22:13:41 +0700

    Edited Navbar Email Template

--- src/components/stables/Navbar/NavbarBackEmailTemplate.jsx
+++ src/components/stables/Navbar/NavbarBackEmailTemplate.jsx
@@ -1,47 +1,78 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 
-import NavbarBack from "./NavbarBack";
-import paths from "../../../pages/paths";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBack from "@material-ui/icons/ArrowBack";
 import AddIcon from "@material-ui/icons/Add";
 import EmailIcon from "@material-ui/icons/Email";
 
-class NavbarBackEmailTemplate extends React.Component {
-  render() {
-    const {classes, ...otherProps} = this.props;
+import paths from "../../../pages/paths";
 
-    return (
-      <NavbarBack
-        {...otherProps}
-        Content={
-          <React.Fragment>
-            <Button
-              variant="contained"
-              color="primary"
-              size="small"
-              to={paths.CRM_EMAIL_TEMPLATE_CREATE}
-              component={Link}
-            >
-              <AddIcon />
-              Buat Templat
-            </Button>
-            <Button
-              variant="outlined"
-              color="secondary"
-              size="small"
-              style={{marginLeft: 8}}
-              to={paths.CRM_MAILER}
-              component={Link}
-            >
-              <EmailIcon />
-              Email Blaster
-            </Button>
-          </React.Fragment>
-        }
-      />
-    );
-  }
+const styles = theme => ({
+  appbar: {
+    top: 0,
+    backgroundColor: theme.palette.background.paper,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+});
+
+function NavbarBackEmailTemplate({ classes, history, title, Content }) {
+  return (
+    <AppBar position="static" className={classes.appbar}>
+      <Toolbar>
+        <IconButton
+          className={classes.menuButton}
+          aria-label="Menu"
+          to={paths.CRM_MAILER}
+          component={Link}
+        >
+          <ArrowBack />
+        </IconButton>
+        {title && (
+          <Typography variant="h6" className={classes.grow}>
+            {title}
+          </Typography>
+        )}
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          to={paths.CRM_EMAIL_TEMPLATE_CREATE}
+          component={Link}
+        >
+          <AddIcon />
+          Buat Templat
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default NavbarBackEmailTemplate;
+NavbarBackEmailTemplate.propTypes = {
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+  classes: PropTypes.shape().isRequired,
+  title: PropTypes.string,
+  Content: PropTypes.func,
+};
+
+NavbarBackEmailTemplate.defaultProps = {
+  title: "",
+  Content: null,
+};
+
+export default withRouter(withStyles(styles)(NavbarBackEmailTemplate));
