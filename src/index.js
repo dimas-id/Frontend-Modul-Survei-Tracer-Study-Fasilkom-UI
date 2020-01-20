df90a00fc77222fcbb9ca10e5a226168081ae706
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2020-01-20 18:17:53 +0700

    turn off service worker

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./styles/bootstrap";
 import "./index.css";
 import App from "./App";
 import { isDevelopment } from "./config";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 if (!isDevelopment) {
   Sentry.init({
@@ -22,4 +22,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
