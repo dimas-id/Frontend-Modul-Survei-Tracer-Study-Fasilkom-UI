3c052b6b9909e1f9b53f4afe7d0c23002a208af6
I Made Indra Mahaarta <i.made023@ui.ac.id>
2023-05-16 21:36:33 +0700

    Implementasi Fungsionalitas Buat Survei dan Revamp UI Buat Survei

--- src/components/Pertanyaan/index.js
+++ src/components/Pertanyaan/index.js
@@ -1,12 +1,13 @@
 import classes from "./styles.module.css";
 import CardPertanyaan from "./CardPertanyaan";
 import { useEffect, useState } from "react";
-import { NavbarAuth, NavbarBuatKuesioner } from "../stables/Navbar";
+import { NavbarAuth, NavbarCreateSurvei } from "../stables/Navbar";
 import atlasV3 from "../../modules/api/atlas/v3";
 import Toast from "../Toast/index";
 
 const Pertanyaan = () => {
   const [deskripsi, setDeskripsi] = useState("");
+  const [nama, setNama] = useState("");
   const [listPertanyaan, setListPertanyaan] = useState([
     {
       pertanyaan: "",
@@ -19,6 +20,9 @@ const Pertanyaan = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [namaStatus, setNamaStatus] = useState(true);
   const [deskripsiStatus, setDeskripsiStatus] = useState(true);
+  const [activeNow, setActiveNow] = useState(0);
+  const [newQuestion, setNewQuestion] = useState();
+  const [removeQuestion, setRemoveQuestion] = useState();
 
   const changePertanyaanHandler = (index, pertanyaan) => {
     const newListPertanyaan = listPertanyaan.slice();
@@ -55,31 +59,82 @@ const Pertanyaan = () => {
     setListPertanyaan(newListPertanyaan);
   };
 
+  useEffect(() => {
+    if (removeQuestion) {
+      if (activeNow === 0) {
+        handleClick("deskripsi");
+      } else {
+        console.log("ok");
+        handleClick(activeNow - 1);
+      }
+    }
+
+    setRemoveQuestion(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [removeQuestion]);
+
   const deleteQuestionHandler = e => {
     const index = parseInt(e.target.id);
     const newListPertanyaan = listPertanyaan.slice();
     newListPertanyaan.splice(index, 1);
     setListPertanyaan(newListPertanyaan);
+    setRemoveQuestion(true);
   };
+
+  useEffect(() => {
+    if (newQuestion) {
+      if (activeNow === "deskripsi") {
+        handleClick(0);
+      } else if (activeNow !== undefined) {
+        handleClick(parseInt(activeNow) + 1);
+      } else if (activeNow === undefined) {
+        handleClick(listPertanyaan.length - 1);
+      }
+    }
+
+    setNewQuestion(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [newQuestion]);
 
   const tambahPertanyaanHandler = () => {
     const newListPertanyaan = listPertanyaan.slice();
-    newListPertanyaan.push({
-      pertanyaan: "",
-      tipe: "Jawaban Singkat",
-      required: true,
-      status: true,
-      option: {},
-    });
+    if (activeNow === undefined) {
+      newListPertanyaan.push({
+        pertanyaan: "",
+        tipe: "Jawaban Singkat",
+        required: true,
+        status: true,
+        option: {},
+      });
+    } else if (activeNow === "deskripsi") {
+      newListPertanyaan.splice(0, 0, {
+        pertanyaan: "",
+        tipe: "Jawaban Singkat",
+        required: true,
+        status: true,
+        option: {},
+      });
+    } else {
+      newListPertanyaan.splice(parseInt(activeNow) + 1, 0, {
+        pertanyaan: "",
+        tipe: "Jawaban Singkat",
+        required: true,
+        status: true,
+        option: {},
+      });
+    }
     setListPertanyaan(newListPertanyaan);
+    setNewQuestion(true);
   };
 
   const setStatus = index => {
+    setActiveNow(index);
     listPertanyaan[index].status = true;
   };
 
-  const onSubmit = async nama => {
+  const onSubmit = async () => {
     setIsLoading(true);
+    setActiveNow();
     const json = {
       nama: nama,
       deskripsi: deskripsi,
@@ -117,6 +172,38 @@ const Pertanyaan = () => {
     }
   };
 
+  const hanldeUp = () => {
+    if (activeNow !== 0) {
+      const newListPertanyaan = listPertanyaan.slice();
+      [newListPertanyaan[activeNow], newListPertanyaan[activeNow - 1]] = [
+        newListPertanyaan[activeNow - 1],
+        newListPertanyaan[activeNow],
+      ];
+      setListPertanyaan(newListPertanyaan);
+      handleClick(activeNow - 1);
+    }
+  };
+
+  const hanldeDown = () => {
+    if (activeNow !== listPertanyaan.length - 1) {
+      const newListPertanyaan = listPertanyaan.slice();
+      [newListPertanyaan[activeNow], newListPertanyaan[activeNow + 1]] = [
+        newListPertanyaan[activeNow + 1],
+        newListPertanyaan[activeNow],
+      ];
+      setListPertanyaan(newListPertanyaan);
+      handleClick(activeNow + 1);
+    }
+  };
+
+  const handleClick = idx => {
+    try {
+      const element = document.getElementById(idx);
+      element.scrollIntoView({ behavior: "smooth" });
+    } catch (e) {}
+    setActiveNow(idx);
+  };
+
   useEffect(() => {
     Toast(
       "Silakan mengisi survei, pastikan seluruh field terisi",
@@ -126,22 +213,23 @@ const Pertanyaan = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{ height: "100vh", overflow: "hidden" }}>
       <NavbarAuth title="Buat Kuesioner" />
-      <NavbarBuatKuesioner
-        onSubmit={onSubmit}
-        isLoading={isLoading}
-        namaStatus={namaStatus}
-        setNamaStatus={setNamaStatus}
-      />
+      <NavbarCreateSurvei onSubmit={onSubmit} isLoading={isLoading} />
       <div className={classes.pertanyaan}>
         <div className={classes["no-pertanyaan"]}>
-          <div className={classes["no-pertanyaan-div"]}>
+          <div className={classes["no-pertanyaan-wrapper"]}>
             {listPertanyaan.map((el, idx) => {
               return (
                 <div
                   key={idx}
-                  className={`${classes.no} ${el.status ? "" : classes.red}`}
+                  onClick={() => {
+                    setStatus(idx);
+                    handleClick(idx);
+                  }}
+                  className={`${classes.no} ${
+                    el.status ? "" : classes.red
+                  } ${activeNow === idx && classes.activeNow}`}
                 >
                   {parseInt(idx) + 1}
                 </div>
@@ -150,41 +238,76 @@ const Pertanyaan = () => {
           </div>
         </div>
         <div className={classes["pertanyaan-wrapper"]}>
-          <div
-            className={`${classes.card} ${classes.description} ${
-              deskripsiStatus ? "" : classes.red
-            }`}
-          >
-            {deskripsiStatus === false && (
-              <div>
-                <span
-                  style={{
-                    paddingLeft: "12px",
-                    color: "red",
-                    fontSize: "12px",
-                  }}
-                >
-                  *wajib diisi
-                </span>
+          <div style={{ position: "relative" }}>
+            <div
+              id="deskripsi"
+              onClick={() => {
+                setActiveNow("deskripsi");
+                setDeskripsiStatus(true);
+                setNamaStatus(true);
+              }}
+              className={`${classes.card} ${classes.description} ${activeNow ===
+                "deskripsi" && classes.green} ${
+                deskripsiStatus ? "" : classes.red
+              }`}
+            >
+              {(deskripsiStatus === false || namaStatus === false) && (
+                <div>
+                  <span
+                    style={{
+                      paddingLeft: "12px",
+                      color: "red",
+                      fontSize: "12px",
+                    }}
+                  >
+                    *wajib diisi
+                  </span>
+                </div>
+              )}
+              <h3>Nama Survei</h3>
+              <input
+                placeholder="Masukkan nama survei disini"
+                className={classes.input}
+                type="text"
+                style={{
+                  marginBottom: "20px",
+                  width: "100%",
+                  maxWidth: "100%",
+                }}
+                onChange={e => {
+                  setNama(e.target.value);
+                  setNamaStatus(true);
+                }}
+              />
+              <h3>Deskripsi</h3>
+              <textarea
+                rows={5}
+                className={classes.textarea}
+                placeholder="Masukkan deskripsi disini"
+                value={deskripsi}
+                onChange={e => {
+                  setDeskripsi(e.target.value);
+                  setDeskripsiStatus(true);
+                }}
+              />
+            </div>
+            {activeNow === "deskripsi" && (
+              <div className={classes.actionbtn}>
+                <button onClick={tambahPertanyaanHandler}>
+                  <img
+                    src="https://i.ibb.co/HFpDT9z/icons8-add-67.png"
+                    alt="add icon"
+                    style={{ width: "30px" }}
+                  />
+                </button>
               </div>
             )}
-            <h3>Deskripsi</h3>
-            <textarea
-              rows={5}
-              className={classes.textarea}
-              placeholder="Masukkan deskripsi disini"
-              value={deskripsi}
-              style={{ fontSize: "16px" }}
-              onChange={e => {
-                setDeskripsi(e.target.value);
-                setDeskripsiStatus(true);
-              }}
-            />
           </div>
           {listPertanyaan.map((el, idx) => {
             return (
-              <div key={idx}>
+              <div style={{ width: "100%", position: "relative" }} key={idx}>
                 <CardPertanyaan
+                  activeNow={activeNow}
                   index={idx}
                   pertanyaan={el.pertanyaan}
                   status={el.status}
@@ -198,14 +321,34 @@ const Pertanyaan = () => {
                   deleteQuestionHandler={deleteQuestionHandler}
                   setStatus={setStatus}
                 />
+                {activeNow === idx && (
+                  <div className={classes.actionbtn}>
+                    <button onClick={tambahPertanyaanHandler}>
+                      <img
+                        src="https://i.ibb.co/HFpDT9z/icons8-add-67.png"
+                        alt="add icon"
+                        style={{ width: "30px" }}
+                      />
+                    </button>
+                    <button onClick={hanldeUp}>
+                      <img
+                        src="https://i.ibb.co/sHMgb2w/icons8-down-67-copy.png"
+                        alt="up img"
+                        style={{ width: "30px" }}
+                      />
+                    </button>
+                    <button onClick={hanldeDown}>
+                      <img
+                        src="https://i.ibb.co/j5dPjJT/icons8-down-67.png"
+                        alt="down img"
+                        style={{ width: "30px" }}
+                      />
+                    </button>
+                  </div>
+                )}
               </div>
             );
           })}
-          <div className={classes["add-pertannyaan"]}>
-            <button onClick={tambahPertanyaanHandler}>
-              Tambahkan Pertanyaan
-            </button>
-          </div>
         </div>
       </div>
     </div>
