0993a0a10ccfd663ac2da26df1448899cb33369a
dimas-id <dimas.ilham01.ui.ac.id>
2023-05-25 22:52:13 +0700

    fixing email blaster-related API

--- src/modules/api/atlas/v3/email-blaster.js
+++ src/modules/api/atlas/v3/email-blaster.js
@@ -14,12 +14,13 @@ export default Object.freeze({
       surveiId,
       recipients,
     }),
-  getEmailRecipients: (survei_id, group_recipients_years, group_recipients_terms, individual_recipients_emails) =>
+  getEmailRecipients: (survei_id, group_recipients_years, group_recipients_terms, individual_recipients_emails, csv_emails) =>
     http.post(`${API_V3_URL}/recipients/get-all`, {
       survei_id,
       group_recipients_years, 
       group_recipients_terms, 
       individual_recipients_emails,
+      csv_emails
     }),
   getGroupTotal: (survei_id, year, term) =>
     http.post(`${API_V3_URL}/recipients/group-total`, {
@@ -28,7 +29,5 @@ export default Object.freeze({
       term
     }),
   uploadEmailCsv: (csv_files) =>
-    http.post(`${API_V3_URL}/recipients/csv/upload`, {
-      csv_files: csv_files,
-    }),
+    http.post(`${API_V3_URL}/recipients/csv/upload`, csv_files ),
 });
